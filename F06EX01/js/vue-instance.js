e9f0399f19bd7bd4a896b3353b3459e7c07d207a
Ricardo Alexandre Peixoto de Queirós <ricardoqueiros@eseig.pt>
2018-10-19 16:41:56 +0100

    F06EX01 solved!

--- F06EX01/js/vue-instance.js
+++ F06EX01/js/vue-instance.js
@@ -1,37 +1,44 @@
-
 const vm = new Vue({
     el: "#intro",
     data: {
         task: "",
         type: "personal",
-        filterType: "all",
+        filter: "all",
         tasks: []
     },
     created() {
-        if (localStorage.tasks) {
-            this.tasks = JSON.parse(localStorage.getItem("tasks"))    
-        }        
+        // Add listener to save tasks only when the tab closes
+        window.addEventListener('unload', this.saveStorage)
+        // Get tasks from the localstorage
+        if (localStorage.getItem("tasks")) {
+            this.tasks = JSON.parse(localStorage.getItem("tasks"))
+        }
     },
-    methods: {
-        addTask() {
 
-            this.tasks.push({ name: this.task, type: this.type })
-           
+    methods: {
+        // Add unique tasks
+        addTask() {
+            if (!this.tasks.some((task) => task.name === this.task)) {
+                this.tasks.push({ name: this.task, type: this.type })
+            } else {
+                alert("The task is already added!")
+            }
         },
+        // Remove task by its position in the array
         removeTask(index) {
             this.tasks.splice(index, 1)
+        },
+        // Save tasks in the localstorage
+        saveStorage() {            
+            localStorage.setItem("tasks",JSON.stringify(this.tasks))
         }
     },
     computed: {
+        // Returns a new array of tasks based on the user's filter
         filteredTasks() {
             return this.tasks.filter(
-                (task) => task.type === this.filterType || this.filterType == "all"
+                (task) => task.type === this.filter || this.filter == "all"
             )
         }
     }
 })
-
-window.onunload = function () {    
-     // Save local storage
-     localStorage.setItem("tasks",JSON.stringify(this.tasks))
-};

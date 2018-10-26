1a67baabc5ec0080d5bfdc2c358b48407661b0f3
Ricardo Alexandre Peixoto de Queirós <ricardoqueiros@eseig.pt>
2018-10-26 16:44:22 +0100

    Filter and Order features added!

--- F07EX01/js/vue-instance.js
+++ F07EX01/js/vue-instance.js
@@ -1,44 +1,89 @@
 const vm = new Vue({
     el: "#intro",
     data: {
-    id:0, 
-       continent: "all",
-       country:"",
-       cities:"",
-       description:"",
-       startDate:"",
-       endDate:"",
-       type:"holidays",
-       photo:"",
-       travels: []
+        id: 0,
+        continent: "all",
+        country: "",
+        cities: "",
+        description: "",
+        startDate: "",
+        endDate: "",
+        type: "holidays",
+        photo: "",
+        filterContinent:"",
+        filterStartDate: "",
+        filterEndDate: "",
+        travels: []
     },
     methods: {
+        // Get last travel id
+        getLastId() {
+            return this.travels.length ? this.travels[this.travels.length - 1].id : 0
+        },
         // Add travel
         addTravel() {
             this.travels.push({
-                id:this.travels.length + 1,
+                id: this.getLastId() + 1,
                 continent: this.continent,
                 country: this.country,
                 cities: this.cities,
                 description: this.description,
-                startDate: this.startDate,
-                endDate: this.endDate,
+                startDate: new Date(this.startDate),
+                endDate: new Date(this.endDate),
                 type: this.type,
                 photo: this.photo
             })
         },
-         // Remove travel based on its ID
-         removeTravel(id) {
-            this.travels = this.travels.filter( 
+        // Remove travel based on its ID
+        removeTravel(id) {
+            this.travels = this.travels.filter(
                 (travel) => travel.id !== id
-            )            
+            )
         },
+
+        // Order by country
+        compareCountry(a, b) {
+            if(a.country < b.country) return -1
+            if(a.country > b.country) return 1
+            else return 0
+        },
+        orderByCountry() {
+            this.travels.sort(this.compareCountry)
+        },
+        // Order by start date
+        compareDate(a, b) {
+            if(a.startDate < b.startDate) return -1
+            if(a.startDate > b.startDate) return 1
+            else return 0
+        },
+        orderByDate() {
+            this.travels.sort(this.compareDate)
+        }
     },
     computed: {
-        // Returns a new array of travel based on the user's filter
+        // Returns a new array of travels based on the user's filter
         filteredTravels() {
             return this.travels.filter(
-                (travel) => travel.continent === this.continent || this.continent == "all"
+                (travel) => {
+                    let filterContinentResult = true
+                    let filterDatesResult = true
+                    
+                    // Filter continent
+                    if(this.filterContinent!=="") {
+                        filterContinentResult = travel.continent === this.filterContinent      
+                    } 
+
+                    // Filter dates                    
+                    if(this.filterStartDate!=="" && this.filterEndDate!=="") {
+                        filterDatesResult = 
+                            travel.startDate >= new Date(this.filterStartDate) && 
+                            travel.startDate <= new Date(this.filterEndDate)
+                    }
+
+                    // return the conjunction of the two filters
+                    return filterContinentResult && filterDatesResult 
+
+                }
             )
         }
     }

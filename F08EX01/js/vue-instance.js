dcfda61d880881fb71b67fa3c07585aedf913af5
unknown <ricardo.queiros@gmail.com>
2018-11-01 18:57:28 +0000

    F08EX01 - A/B/C/D questions solved!

---
+++ F08EX01/js/vue-instance.js
@@ -0,0 +1,108 @@
+const vm = new Vue({
+    el: "#intro",
+    data: {   
+        index: 0,     
+        castles: [],
+        form : {
+            name: "",
+            link: "",
+            editName: "",
+            editLink:"",
+            filterName:""            
+        }
+    },
+    created() {       
+        this.castles = [
+            {id: 1, name: "Castelo de Bragança", link: "https://i1.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/11021478_789330187780883_1992398901395585075_o.jpg?resize=640%2C415", year:1642},
+            {id: 2, name: "Castelo de Almourol", link: "https://i1.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/11046854_793447744035794_6644364760515171458_o.jpg?resize=640%2C415", year:1642},
+            {id: 3, name: "Castelo de Marvão", link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/10649042_702725743107995_1458143537900302356_o.jpg?resize=640%2C415", year:1642},
+            {id: 4, name: "Castelo de Montalegre", link: "https://i1.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/11021309_786097224770846_4490230410016196052_o-1-e1485957738973.jpg?resize=640%2C415", year:1642},
+            {id: 5, name: "Castelo de Sortelha", link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/1510562_743660549014514_1798999603922685904_n.jpg?resize=640%2C415", year:1642},
+            {id: 6, name: "Castelo de Arraiolos", link: "https://i0.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/10981073_787377341309501_2130083883941642881_o.jpg?resize=640%2C415", year:1642},
+            {id: 7, name: "Castelo de Santa Maria da Feira", link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/10993133_785137361533499_2834769696870331489_o-1.jpg?resize=640%2C415", year:1642},
+            {id: 8, name: "Castelo de Lindoso", link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/10854966_780229555357613_3783697791888632461_o.jpg?resize=640%2C415", year:1642},
+            {id: 10, name: "Castelo de Guimarães", link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/casteloguim-1-e1446910277358.jpg?resize=640%2C415", year:1642}
+        ]    
+    },
+
+  
+    methods: {
+         
+        // Get last castle id
+        getLastId() {
+            return this.castles.length ? this.castles[this.castles.length - 1].id : 0
+        },
+
+        getIndexById(id) {
+            return this.castles.findIndex(
+                (castle) => castle.id === id
+            )
+        },
+
+        // Add castle
+        addCastle() {
+            this.castles.push({
+                id: Number(this.getLastId()) + 1,
+                name: this.form.name,
+                link: this.form.link
+            })
+        },        
+        // Edit castle based on its ID
+        editCastle(id) {
+            document.getElementById("editCastleDialog").showModal()
+            this.index = this.getIndexById(id) 
+            this.form.editName = this.castles[this.index].name
+            this.form.editLink = this.castles[this.index].link
+
+            
+        },
+        updateCastle() {            
+            this.castles[this.index].name = this.form.editName  
+            this.castles[this.index].link = this.form.editLink             
+            document.getElementById("editCastleDialog").close()
+        },
+
+        // View castle based on its ID
+        viewCastle(id) {
+            this.index = this.getIndexById(id)            
+            this.form.link = this.castles[this.index].link
+            document.getElementById("viewCastleDialog").showModal()
+            
+        },
+        // Close view of the castle
+        closeViewCastle() {
+            document.getElementById("viewCastleDialog").close()
+        },
+
+        // Remove castle based on its ID
+        removeCastle(id) {
+            this.castles = this.castles.filter(
+                (castle) => castle.id !== id
+            )
+        },
+          // Order by name
+          compareName(a, b) {
+            if(a.name < b.name) return -1
+            if(a.name > b.name) return 1
+            else return 0
+        },
+        orderByName() {
+            this.castles.sort(this.compareName)
+        },       
+    },
+    computed: {
+        // Returns a new array of castles based on the user's filter
+        filteredCastles() {
+            return this.castles.filter(
+                (castle) => {
+                    let filterContinentResult = true                                        
+                    if(this.form.filterName!=="") {
+                        filterContinentResult = castle.name.includes(this.form.filterName)
+                    }                     
+                    return filterContinentResult 
+                }
+            )
+        }    
+
+    }
+})

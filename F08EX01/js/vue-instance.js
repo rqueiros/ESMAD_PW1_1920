0315adec8a70fb83196ddb22d05221c6319a2446
rqueiros <ricardo.queiros@gmail.com>
2018-11-10 16:26:06 +0000

    Last update

--- F08EX01/js/vue-instance.js
+++ F08EX01/js/vue-instance.js
@@ -13,65 +13,65 @@ const vm = new Vue({
     },
     created() {
         this.castles = [{
-            id: 1,
-            name: "Castelo de Bragança",
-            link: "https://i1.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/11021478_789330187780883_1992398901395585075_o.jpg?resize=640%2C415",
-            year: 1000
-        },
-        {
-            id: 2,
-            name: "Almourol",
-            link: "https://i1.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/11046854_793447744035794_6644364760515171458_o.jpg?resize=640%2C415",
-            year: 1000
-        },
-        {
-            id: 3,
-            name: "Castelo de Marvão",
-            link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/10649042_702725743107995_1458143537900302356_o.jpg?resize=640%2C415",
-            year: 1000
-        },
-        {
-            id: 4,
-            name: "Castelo de Montalegre",
-            link: "https://i1.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/11021309_786097224770846_4490230410016196052_o-1-e1485957738973.jpg?resize=640%2C415",
-            year: 1000
-        },
-        {
-            id: 5,
-            name: "Castelo de Sortelha",
-            link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/1510562_743660549014514_1798999603922685904_n.jpg?resize=640%2C415",
-            year: 1000
-        },
-        {
-            id: 6,
-            name: "Castelo de Arraiolos",
-            link: "https://i0.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/10981073_787377341309501_2130083883941642881_o.jpg?resize=640%2C415",
-            year: 1000
-        },
-        {
-            id: 7,
-            name: "Castelo de Santa Maria da Feira",
-            link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/10993133_785137361533499_2834769696870331489_o-1.jpg?resize=640%2C415",
-            year: 1000
-        },
-        {
-            id: 8,
-            name: "Castelo de Lindoso",
-            link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/10854966_780229555357613_3783697791888632461_o.jpg?resize=640%2C415",
-            year: 1000
-        },
-        {
-            id: 9,
-            name: "Castelo dos Mouros",
-            link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/casteloguim-1-e1446910277358.jpg?resize=640%2C415",
-            year: 1000
-        },
-        {
-            id: 10,
-            name: "Castelo de Guimarães",
-            link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/casteloguim-1-e1446910277358.jpg?resize=640%2C415",
-            year: 1000
-        }
+                id: 1,
+                name: "Castelo de Bragança",
+                link: "https://i1.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/11021478_789330187780883_1992398901395585075_o.jpg?resize=640%2C415",
+                year: 1000
+            },
+            {
+                id: 2,
+                name: "Almourol",
+                link: "https://i1.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/11046854_793447744035794_6644364760515171458_o.jpg?resize=640%2C415",
+                year: 1000
+            },
+            {
+                id: 3,
+                name: "Castelo de Marvão",
+                link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/10649042_702725743107995_1458143537900302356_o.jpg?resize=640%2C415",
+                year: 1000
+            },
+            {
+                id: 4,
+                name: "Castelo de Montalegre",
+                link: "https://i1.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/11021309_786097224770846_4490230410016196052_o-1-e1485957738973.jpg?resize=640%2C415",
+                year: 1000
+            },
+            {
+                id: 5,
+                name: "Castelo de Sortelha",
+                link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/1510562_743660549014514_1798999603922685904_n.jpg?resize=640%2C415",
+                year: 1000
+            },
+            {
+                id: 6,
+                name: "Castelo de Arraiolos",
+                link: "https://i0.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/10981073_787377341309501_2130083883941642881_o.jpg?resize=640%2C415",
+                year: 1000
+            },
+            {
+                id: 7,
+                name: "Castelo de Santa Maria da Feira",
+                link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/10993133_785137361533499_2834769696870331489_o-1.jpg?resize=640%2C415",
+                year: 1000
+            },
+            {
+                id: 8,
+                name: "Castelo de Lindoso",
+                link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/10854966_780229555357613_3783697791888632461_o.jpg?resize=640%2C415",
+                year: 1000
+            },
+            {
+                id: 9,
+                name: "Castelo dos Mouros",
+                link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/casteloguim-1-e1446910277358.jpg?resize=640%2C415",
+                year: 1951
+            },
+            {
+                id: 10,
+                name: "Castelo de Guimarães",
+                link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/casteloguim-1-e1446910277358.jpg?resize=640%2C415",
+                year: 1000
+            }
         ]
     },
 
@@ -155,7 +155,7 @@ const vm = new Vue({
                     this.castles = this.castles.map(
                         (castle) => {
                             if (castle.name.indexOf(str) !== -1) {
-                                castle.name = castle.name.substring(castle.name.indexOf(str) + str.length)
+                                castle.name = castle.name.replace(str, "")
                             }
                             return castle
                         }
@@ -200,18 +200,47 @@ const vm = new Vue({
                     ))
                     break;
                 case "fi":
-                    this.castles = this.castles.map(
+                    const separator = "-"
+                    console.log(this.castles.map(
                         (castle) => castle.name
-                    )
-                    console.log(this.castles.join("-"))
+                    ).join(separator))
                     break;
                 case "fii":
-                    this.castles = this.castles.filter(
-                        (castle) => castle.year > 1950                        
-                    )
-                    console.log(this.castles)
+                    const modernCastlesYear = 1950
+                    console.log(this.castles.filter(
+                        (castle) => {
+                            if (castle.year > modernCastlesYear) {
+                                return castle.name
+                            }
+                        }
+                    ))
                     break;
-
+                case "fiii":
+                    const resetLinksYear = 1000
+                    console.log(this.castles.map(
+                        (castle) => {
+                            if (castle.year < resetLinksYear) {
+                                castle.link = ""
+                            }
+                            return castle
+                        }
+                    ))
+                    break;
+                case "fiv":
+                    const incrementsYear = 5
+                    console.log(this.castles.map(
+                        (castle) => {
+                            if (castle.name.toLowerCase().startsWith("a") ||
+                                castle.name.toLowerCase().startsWith("e") ||
+                                castle.name.toLowerCase().startsWith("i") ||
+                                castle.name.toLowerCase().startsWith("o") ||
+                                castle.name.toLowerCase().startsWith("u")) {
+                                castle.year += incrementsYear
+                            }
+                            return castle
+                        }
+                    ))
+                    break;
                 default:
                     break;
             }
